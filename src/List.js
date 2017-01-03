4b37708526151890aaecd3d20275d59cac0629a9
Lilia <geertszelia@gmail.com>
2017-01-03 16:12:44 +0100

    Enregistrement de la valeur de l'input + reconnaissance submit data

--- src/List.js
+++ src/List.js
@@ -2,20 +2,41 @@ import React, {Component} from 'react';
 import './List.css';
 
 class List extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {items: [], value:''};
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
 	render() {
 		return (
 			<div className="list">
-            	<h4>Ajouter un nouveau truc</h4>
-            	<input type="text" placeholder="truc"/>
-            	<button>+</button>
-            	<ul>
-					<li>Test 1</li>
-					<li>Test 2</li>
-					<li>Test 3</li>
-            	</ul>
+            	<form onSubmit={this.handleSubmit}>
+            		<label>
+            			Ajouter un truc à faire
+	            		<input type="text" placeholder="truc" value={this.state.value} onChange={this.handleChange}/>
+	            	</label>
+	            	<button>+</button>
+	            </form>
+				<ul>
+	    	   		{this.state.items.map(item =>(
+						<li key={item.id}>{item.value}</li>
+					))};
+	        	</ul>
           	</div>
 		);
 	}
+
+	handleChange(e) {
+		this.setState({value: e.target.value});
+	}
+
+	handleSubmit(e) {
+		e.preventDefault();
+		console.log(this.state.value);
+	}
 }
+
 
 export default List;
